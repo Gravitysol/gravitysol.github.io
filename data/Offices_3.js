e3f42bdc8df7f51f593a54584eba98784dce6a92
Gravitysol <usmani2nice@gmail.com>
2023-02-06 16:27:27 +0500

    test
    Testpage

---
+++ data/Offices_3.js
@@ -0,0 +1,9 @@
+var json_Offices_3 = {
+"type": "FeatureCollection",
+"name": "Offices_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0, "Address": "901 S MoPac Expressway, Building 1, Suite #300 Austin, TX 78746" }, "geometry": { "type": "Point", "coordinates": [ -97.780243759762968, 30.261361547965439 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Address": "800 Industrial Blvd, STE 100 Grapevine, TX 76051" }, "geometry": { "type": "Point", "coordinates": [ -97.105233333333302, 32.930788253263266 ] } }
+]
+}
---
+++ data/Ports_2.js
@@ -0,0 +1,12 @@
+var json_Ports_2 = {
+"type": "FeatureCollection",
+"name": "Ports_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0, "Name": "Inland Port of Greer", "Link": "https:\/\/scspa.com\/sc-ports-locations\/inland-port-greer" }, "geometry": { "type": "Point", "coordinates": [ -82.189905374957505, 34.931762730826946 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Name": "Port of Long Beach", "Link": "https:\/\/polb.com\/" }, "geometry": { "type": "Point", "coordinates": [ -118.216445896999915, 33.754180143000028 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Name": "Port of Savannah", "Link": "https:\/\/gaports.com\/facilities\/port-of-savannah" }, "geometry": { "type": "Point", "coordinates": [ -81.104584116999931, 32.091705494000053 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Name": "Port of Charleston", "Link": "https:\/\/scspa.com\/" }, "geometry": { "type": "Point", "coordinates": [ -79.914556416999901, 32.775654010000039 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Name": "Port of Houston", "Link": "https:\/\/porthouston.com\/" }, "geometry": { "type": "Point", "coordinates": [ -95.328211046999897, 29.759838524000095 ] } }
+]
+}
